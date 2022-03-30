67bf724b5c7fe04c8c7dfc7d23176010555a9abc
Henri0812 <p.metzner@gmx.at>
2022-03-31 01:53:05 +0200

    progress

--- Logic.js
+++ Logic.js
@@ -1,3 +1,8 @@
+var AllBotsTemplate = [{rarity : "common"}, {rarity : "rare"}, {rarity : "epic"}, {rarity : "void"}, 
+                        {faction : "neutral"}, {faction : "lacrean"}, {faction : "illskagaard"}, {faction : "arboria"},
+                        {type : "neutral baby"}, {type : "neutral baby"}, {type : "fem large"}, {type : "male large"},
+                        {botclass : "tank"}, {botclass : "melee dps"}, {botclass : "ranged dps"}, {botclass : "support"}];
+
 function AsyncTextReader(Place) {
     return new Promise(function (resolve, reject) {
         var objXMLhttp = new XMLHttpRequest()
@@ -23,6 +28,7 @@ var GeneArray = [];
 async function DecodeGenes() {
   GeneArray = await AsyncTextReader('./SolarbotsList.txt');
   console.log(GeneArray);
+  console.log(AllBotsTemplate);
 }
 
 function SelectAddress() {
@@ -80,5 +86,62 @@ async function GetAccountData(ETHAddy) {
     console.log(AmountOfBots);
   });
 
+  for(i=0; i<AmountOfBots; i++) {
+    FetchBots(i+1, i, AmountOfBots);
+  }
+
 }
 
+var BotsInAccountArray = [];
+
+async function FetchBots(id, bodydata, AmountOfBots) {
+
+  let BotID = null;
+
+  bodydata = bodydata.toString(16);
+
+  let CorrectAmountOfZeros = "0".repeat(7-bodydata.length);   //gives bodydata data fetch query stuff the correct amount of zeros, since the hex number has different lengths depending on what you enter
+
+  bodydata = "0x2f745c59000000000000000000000000606fd755081d7ea7c1e2d6faafe6d9cdb1ea142c000000000000000000000000000000000000000000000000000000000" + CorrectAmountOfZeros + bodydata;
+
+  await fetch("https://node1.web3api.com/", {
+    "credentials": "omit",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+        "Accept": "*/*",
+        "Accept-Language": "de,en-US;q=0.7,en;q=0.3",
+        "Content-Type": "application/json",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site"
+    },
+    "referrer": "https://etherscan.io/",
+    "body": "{\"jsonrpc\":\"2.0\",\"id\":"+id+",\"method\":\"eth_call\",\"params\":[{\"from\":\"0x0000000000000000000000000000000000000000\",\"data\":\""+bodydata+"\",\"to\":\"0x8009250878ed378050ef5d2a48c70e24eb2ede7e\"},\"latest\"]}",
+    "method": "POST",
+    "mode": "cors"
+  })
+  .then(function(response) { 
+    return response.json(); 
+  })
+            
+  .then(function(data) {
+    BotID = data.result;
+    BotID = BotID.substring(2);
+    BotID = BotID.replace(/^0+/, '');
+    BotID = parseInt(BotID, 16);
+    BotsInAccountArray.push(BotID);
+  })
+  
+  .catch((err) => console.error('[request failed]', err.message));
+
+  if(AmountOfBots == id) {
+    console.log("before sleep");
+    await sleep(2000);
+    console.log(BotsInAccountArray);
+  }
+
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
